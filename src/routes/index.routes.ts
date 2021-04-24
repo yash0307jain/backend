41d3b8509a373c4932f3212f984f2400e9e0f0e5
Pavan Singhal <spavan769@gmail.com>
2021-04-24 17:05:38 +0530

    added signup functionality

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -1,5 +1,7 @@
 import express, { Router } from "express";
+import {loginRoutes} from '../routes/login.routes'
 
 const router: Router = express.Router();
+router.use("/", loginRoutes);
 
 export const indexRoutes = router;
