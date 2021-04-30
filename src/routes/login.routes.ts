716b9a5cdaec440247720565e3285289cacae46b
Yash Jain <yash0307jain@gmail.com>
2021-04-30 15:18:53 +0530

    adding login route

--- src/routes/login.routes.ts
+++ src/routes/login.routes.ts
@@ -5,5 +5,6 @@ const userController = new UserController();
 const router: Router = express.Router();
 
 router.post("/signup", userController.signUp);
+router.post("/signin", userController.signIn);
 
 export const loginRoutes = router;
